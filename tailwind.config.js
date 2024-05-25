5a99fc6b16c5dbc2d9c202388c3644f867bf72de
satorusvessel <hellodarky8@gmail.com>
2024-05-25 10:53:14 +0300

    ✨ feat: finshed responsivness both on Desktop and Mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: 'hsl(47, 88%, 63%)',
+      },
+    },
   },
   plugins: [],
 }
